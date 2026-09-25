59bd39fc1634cd1fb1c6f503057cadcd3fbba925
subhranshu <humblesoullevi.2006@gmail.com>
2026-09-26 02:50:15 +0530

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-
-
-        var result = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
-        var user = result.rows[0];
-        if (!user || !verifyPassword(password, user.password))
-            return res.status(403).json({ message: "Invalid username or password." });
-
-        var token = jwt.sign(
-            { id: String(user.id), username: user.username },
